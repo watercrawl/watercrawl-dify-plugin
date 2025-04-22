b38777cf939488f17dbb317f7ff8f056b4d3e008
zlp <zhanglp@tosho-trading.co.jp>
2025-04-22 12:27:43 +0900

    modify validate_url for ip/localhost

--- provider/water-crawl.py
+++ provider/water-crawl.py
@@ -33,7 +33,13 @@ class WaterCrawlProvider(ToolProvider):
 
     def validate_url(self, url):
         try:
-            regex = r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$"
+            regex = re.compile(
+                    r'^(https?:\/\/)+' # http:// or https://
+                    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                    r'localhost|' #localhost...
+                    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                    r'(?::\d+)?' # optional port
+                    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
             return url if bool(re.match(regex, url)) else None
         except Exception:
             return False
